f11634a408f25ee3fd1b421df06b82448c67a6ca
Roaa Albishri <roaa20101@hotmail.com>
2022-10-30 20:58:28 +0300

    Updated LAB_DJANGO_TEMPLATE_INHERITANCE

--- TemplatesProject/AppTemplate/urls.py
+++ TemplatesProject/AppTemplate/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path     
 from . import views
 
-
+app_name = 'AppTemplate'
 urlpatterns = [
     path('today/', views.today, name="today"),
     path('random/password/', views.password, name="password"),
