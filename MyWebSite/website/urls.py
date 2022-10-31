bbe7dd6ee808680c83387ddce4ad2c30c212f2f2
Mohammed.I.Alharbi <name@gmail.com>
2022-10-31 13:04:11 +0300

    use dynamic urls for my paths

--- MyWebSite/website/urls.py
+++ MyWebSite/website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "MyWebsite"
+
 urlpatterns = [
     path('today/', views.theDate, name = "Date"),
     path('password/', views.passwordGenerator, name = "PassWord"),
